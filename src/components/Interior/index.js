6ac976be7999916848da3a1a80b7b9adffc964be
Nazidev01 <toktonalykyzynazi3675478@gmail.com>
2023-05-23 13:56:41 +0600

    lore

--- src/components/Interior/index.js
+++ src/components/Interior/index.js
@@ -5,6 +5,7 @@ const Interior = () => {
         <div>
             <h1>Nazi</h1>
             <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cupiditate, suscipit.</p>
+            <p>Lorem ipsum dolor sit amet.</p>
         </div>
     );
 };
