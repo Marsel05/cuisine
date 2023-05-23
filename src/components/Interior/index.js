00ffa5af5d5951fd0771724b9c2fb8dfee21af2f
Nazidev01 <toktonalykyzynazi3675478@gmail.com>
2023-05-23 13:52:49 +0600

    lorem

--- src/components/Interior/index.js
+++ src/components/Interior/index.js
@@ -4,6 +4,7 @@ const Interior = () => {
     return (
         <div>
             <h1>Nazi</h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cupiditate, suscipit.</p>
         </div>
     );
 };
